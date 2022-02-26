9714250b13b13afd277197bd82c00ffc17aeaa16
SoNiC <SoNiC@IEB-DEV>
2022-02-26 01:53:38 -0800

    Add project files.

---
+++ SuperHeroApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Models;
+
+namespace SuperHeroApi.Data
+{
+    public class DataContext : DbContext
+    {
+        //Step 2
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        //Step 3 SuperHeroes is the name of tabe in database
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+    
+}
